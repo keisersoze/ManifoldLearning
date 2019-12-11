638748c91c82aa47837460c547c5e2dc7f4bb1f1
Arn <arnoud.piano@gmail.com>
2019-12-11 21:40:59 +0100

    Tidy code
    Aggiunti commenti, preparato il codice per il manifold K-fold, iniziato a scrivere il codice in utils

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn import manifold
 from sklearn import svm
 from sklearn.metrics import accuracy_score
-from sklearn.neighbors import NearestNeighbors
-
+from sklearn.model_selection import KFold
 import math
 
 from grakel import GraphKernel
@@ -15,44 +14,83 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-from datasets_utils import load_shock_dataset
+from datasets_utils import load_shock_dataset,load_ppi_dataset
 from utils import compute_distance_matrix
 
+
 X, y = load_shock_dataset()
+# X, y = load_ppi_dataset()
 
 ## KFOLD
+
+# Shuffle data
 idx = np.random.permutation(len(X))
 X, y = X[idx], y[idx]
 
+# Initialize chosen Kernel
 spk = GraphKernel(kernel={"name": "shortest_path", "with_labels": False}, normalize=True)
 
-from sklearn.model_selection import KFold
-kf = KFold(n_splits=2) # n_splits is 2 for testing reasons
+# Split indexes according to Kfold with k = 10
+k = 10
+kf = KFold(n_splits=k)
 
-scores = []
+# initialize scores lists
+scores1 = []
+scores2 = []
+
+
 for train_index, test_index in kf.split(X):
 
+    # split train and test of K-fold
     X_train, X_test = X[train_index], X[test_index]
     y_train, y_test = y[train_index], y[test_index]
 
+    # Fit and transform train and test with the graph kernel
     K_train = spk.fit_transform(X_train)
     K_test = spk.transform(X_test)
 
-    clf = svm.SVC(kernel='linear', C=1)
-    clf.fit(K_train, y_train)
-    y_pred = clf.predict(K_test)
+    # Initialize and fit classifier for non-embedded graph with test data
+    clf1 = svm.SVC(kernel='linear', C=1)
+    clf1.fit(K_train, y_train)
 
+    # make prediction and calculate accuracy
+    y_pred = clf1.predict(K_test)
     acc = accuracy_score(y_test, y_pred)
-    scores.append(acc)
+    scores1.append(acc)
 
-mean = 0
-print(scores)
-for x in scores:
-    mean += x
-mean /= len(scores)
-print("Accuracy: %0.2f" % (mean) )#, scores.std() * 2))
+    '''
+    D_train = compute_distance_matrix(K_train)
+    D_test = compute_test_distance_matrix (K_train, K_test)
+    embedding = manifold.Isomap(n_neighbors=5, n_components=10, metric="precomputed")
+    E_train = embedding.fit_transform(D_train)
+    E_test = embedding.fit(D_test) # non esiste ancora
+    clf2 = svm.SVC(kernel='linear', C=1)
+    clf2.fit(X_train, y_train)
+    # Predict and test.
+    y_pred = clf2.predict(X_test)
+    # Calculate accuracy of classification.
+    acc = accuracy_score(y_test, y_pred)
+    scores2.append(acc)
+    '''
+# Calculate mean of the scores
+mean1 = 0
+for x in scores1:
+    mean1 += x
+mean1 /= k
+
+'''
+mean2 = 0
+for x in scores2:
+    mean2 += x
+mean2 /= k
+'''
+
+print("Accuracy of K-Fold non-embedded classification: %0.3f" % (mean1) ) #, scores.std() * 2)) # should calculate std for scores
+print("Accuracy of K-Fold embedded classification: NOT YET AVALIABLE") #%0.3f" % (mean2) )
 
 ##END KFOLD
+
+##OLD CODE
 
 shortestPathKernel = GraphKernel(kernel={"name": "shortest_path", "with_labels": False}, normalize=True)
 
@@ -92,7 +130,7 @@ y_pred = clf.predict(X_test)
 # Calculate accuracy of classification.
 acc = accuracy_score(y_test, y_pred)
 
-print("Accuracy:", str(round(acc * 100, 2)))
+print("Accuracy of embedded classification:", str(round(acc * 100, 2)))
 
 # embeddings = manifold.locally_linear_embedding(X, n_neighbors=10, n_components=2)
 
--- utils.py
+++ utils.py
@@ -7,3 +7,10 @@ def compute_distance_matrix(kernel_matrix):
     for (i, j) in np.ndindex(distance_matrix.shape):
         distance_matrix[i, j] = math.sqrt(kernel_matrix[i, i] + kernel_matrix[j, j] - 2 * kernel_matrix[i, j])
     return distance_matrix
+'''
+# WIP...
+def compute_test_distance_matrix(kernel_matrix, test_matrix):
+    res = np.empty(shape=[test_matrix.shape[0],kernel_matrix.shape[0]])
+    for (i,j) in np.ndindex(res.shape):
+        res[i,j] = math.sqrt(kernel_matrix[i, i] + kernel_matrix[j, j] - 2 * kernel_matrix[i, j])
+'''
